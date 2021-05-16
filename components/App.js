aae3af24c59a8dd58b4fb371bb5b641938aecbbd
Steven Smith <kodemaan@gmail.com>
2021-05-16 00:23:35 -0400

    Adding chakra-ui and setting up light and dark themes
    Updating packages and making sure charka-ui is setup with light and dark
    themes, still need to add a lot more boilerplate to make these a
    work-able personal site

--- components/App.js
+++ components/App.js
@@ -1,1 +1,12 @@
-export const App = () => <div>Steves Personal Site</div>
+import { Box, Button, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import React from 'react'
+export const App = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const mainBoxColor = useColorModeValue('gray.800', 'gray.50') 
+  return (
+    <Box>
+      <Button onClick={toggleColorMode}>Change color mode</Button>
+      <Box bg='tomato' color={mainBoxColor}>Test pokemon</Box>
+    </Box>
+  )
+}
